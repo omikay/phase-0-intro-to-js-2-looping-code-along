693162de297cd46ee3488a6cdbcd5630fc00d9ea
Omid Kayhani <o.kayhani@gmail.com>
2023-04-21 18:14:28 +0300

    for loop done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];
+    for(i=0; i < names.length; i++) {
+        message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        messages.push(message);
+    }
+    return messages;
+}
+
+// example usage
+const thanks = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+console.log(thanks);
+
