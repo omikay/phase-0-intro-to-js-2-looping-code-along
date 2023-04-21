cfbdff8b200a24b788a6884eae5cb2a6a4987068
Omid Kayhani <o.kayhani@gmail.com>
2023-04-21 18:23:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // Code your solutions in this file
+
+// for loop
 function writeCards(names, event) {
     const messages = [];
-    for(i=0; i < names.length; i++) {
-        message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+    for(let i=0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
         messages.push(message);
     }
     return messages;
@@ -12,3 +14,13 @@ function writeCards(names, event) {
 const thanks = writeCards(["Charlie", "Samip", "Ali"], "birthday");
 console.log(thanks);
 
+// while loop
+function countDown(number) {
+    let countdown = number;
+    while (countdown >= 0) {
+        console.log(countdown--);
+    }
+}
+
+// example usage
+countDown(10);
